2015c2ef0bb8af61196b6742d8f82fe4c39fbce3
Abraão Souza Ribeiro <abraaohdg@gmail.com>
2018-12-06 18:12:18 -0200

    exemplo

--- src/main/java/com/example/demo/Sender.java
+++ src/main/java/com/example/demo/Sender.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.FanoutExchange;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -10,7 +11,10 @@ public class Sender {
 	@Autowired
 	private AmqpTemplate rabbitTemplate;
 
+	 @Autowired
+	 private FanoutExchange fanoutExchange;
+	 
 	public void send(String message) {
-		this.rabbitTemplate.convertAndSend("fanoutExchange", "", message);
+		this.rabbitTemplate.convertAndSend(fanoutExchange.getName(), "", message);
 	}
 }
--- src/main/java/com/example/demo/SenderConf.java
+++ src/main/java/com/example/demo/SenderConf.java
@@ -1,62 +1,34 @@
 package com.example.demo;
 
-import java.util.Arrays;
-import java.util.List;
-
-import org.springframework.amqp.core.*;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.amqp.core.AnonymousQueue;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.core.Queue;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SenderConf {
-	/*public final static String topicQueue1Name = "fanout.A";
-	public final static String topicQueue2Name = "fanout.A";
-	public final static String topicExchangeName = "fanout.A";*/
-	
-    @Bean(name="Amessage")
-    public Queue AMessage() {
-        return new Queue("fanout.A");
-    }
-	@Bean(name = "Bmessage")
-	public Queue BMessage() {
-		return new Queue("fanout.B");
-	}
+	/*
+	 * public final static String topicQueue1Name = "fanout.A"; public final static
+	 * String topicQueue2Name = "fanout.A"; public final static String
+	 * topicExchangeName = "fanout.A";
+	 */
 
-	@Bean(name = "Cmessage")
-	public Queue CMessage() {
-		return new Queue("fanout.C");
+	@Bean
+	public Queue autoDeleteQueue1() {
+		return new AnonymousQueue();
 	}
 
 	@Bean
 	FanoutExchange fanoutExchange() {
 		return new FanoutExchange("fanoutExchange");
 	}
-	
-
-/*	@Bean
-	public List<Declarable> topicBindings() {
-		Queue topicQueue1 = new Queue(topicQueue1Name, true,false,false);
-
-		TopicExchange topicExchange = new TopicExchange(topicExchangeName);
-		return Arrays.asList(topicQueue1, topicExchange,
-				BindingBuilder.bind(topicQueue1).to(topicExchange).with(topicQueue1Name));
-	}*/
-
 
 	@Bean
-	Binding bindingExchangeA(@Qualifier("Amessage") Queue AMessage, FanoutExchange fanoutExchange) {
-		return BindingBuilder.bind(AMessage).to(fanoutExchange);
-	}
-
-	@Bean
-	Binding bindingExchangeB(@Qualifier("Bmessage") Queue BMessage, FanoutExchange fanoutExchange) {
-		return BindingBuilder.bind(BMessage).to(fanoutExchange);
-	}
-
-	@Bean
-	Binding bindingExchangeC(@Qualifier("Cmessage") Queue CMessage, FanoutExchange fanoutExchange) {
-		return BindingBuilder.bind(CMessage).to(fanoutExchange);
+	public Binding binding1(FanoutExchange fanout, Queue autoDeleteQueue1) {
+		return BindingBuilder.bind(autoDeleteQueue1).to(fanout);
 	}
 
 }
